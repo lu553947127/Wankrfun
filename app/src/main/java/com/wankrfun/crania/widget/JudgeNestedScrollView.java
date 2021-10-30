71527929e055a9a3cac8c8d86611d07d86543e95
程序鹿 <18866619695@163.com>
2021-10-30 10:37:07 +0800

    第十三次提交

---
+++ app/src/main/java/com/wankrfun/crania/widget/JudgeNestedScrollView.java
@@ -0,0 +1,71 @@
+package com.wankrfun.crania.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+import androidx.core.widget.NestedScrollView;
+
+import com.blankj.utilcode.util.LogUtils;
+
+/**
+ * @ProjectName: Wankrfun
+ * @Package: com.wankrfun.crania.widget
+ * @ClassName: JudgeNestedScrollView
+ * @Description: 解决NestedScrollView多层嵌套滑动冲突
+ * @Author: 鹿鸿祥
+ * @CreateDate: 7/23/21 2:31 PM
+ * @UpdateUser: 更新者
+ * @UpdateDate: 7/23/21 2:31 PM
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class JudgeNestedScrollView extends NestedScrollView {
+    private boolean isNeedScroll = true;
+    private float xDistance, yDistance, xLast, yLast;
+    private int scaledTouchSlop;
+
+    public JudgeNestedScrollView(Context context) {
+        super(context, null);
+    }
+
+    public JudgeNestedScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
+
+    public JudgeNestedScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        scaledTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                xDistance = yDistance = 0f;
+                xLast = ev.getX();
+                yLast = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final float curX = ev.getX();
+                final float curY = ev.getY();
+
+                xDistance += Math.abs(curX - xLast);
+                yDistance += Math.abs(curY - yLast);
+                xLast = curX;
+                yLast = curY;
+                LogUtils.e("SiberiaDante", "xDistance ：" + xDistance + "---yDistance:" + yDistance);
+                return !(xDistance >= yDistance || yDistance < scaledTouchSlop) && isNeedScroll;
+
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    /*
+    该方法用来处理NestedScrollView是否拦截滑动事件
+     */
+    public void setNeedScroll(boolean isNeedScroll) {
+        this.isNeedScroll = isNeedScroll;
+    }
+}
